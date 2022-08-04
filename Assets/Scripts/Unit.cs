4b9a221078ffd837bfed164f2d31fa8c53fa0175
Mahmoud0090 <mmh.00090@gmail.com>
2022-08-04 04:55:24 +0300

    Unit script

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 targetPosition;
+
+    void Update()
+    {
+        float stoppingDistance = .1f;
+
+        if(Vector3.Distance(transform.position , targetPosition) > stoppingDistance)
+        {
+            Vector3 direction = (targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += direction * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Move(new Vector3(4, 0, 4));
+        }
+    }
+
+
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+}
