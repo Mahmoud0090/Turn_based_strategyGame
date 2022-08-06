1327cd4dc8e5f44f9e988decd1753b3ffdd7b84a
Mahmoud0090 <mmh.00090@gmail.com>
2022-08-06 20:14:44 +0300

    Unit Animator Parameter

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
 
     void Update()
@@ -15,6 +16,11 @@ public class Unit : MonoBehaviour
             Vector3 direction = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += direction * moveSpeed * Time.deltaTime;
+            unitAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            unitAnimator.SetBool("IsWalking", false);
         }
 
         if (Input.GetMouseButtonDown(0))
